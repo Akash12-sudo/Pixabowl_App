ea7e9bfe9810e50e520e362e9b06f8dceb6d7140
Akash_007 <60813279+Akash12-sudo@users.noreply.github.com>
2023-12-02 22:12:58 +0530

    Update Firebase.js

--- components/auth/Firebase.js
+++ components/auth/Firebase.js
@@ -10,18 +10,6 @@ import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDTZSt-9PxjFdqABJMZpof_Wf4-MOS40MM",
-//   authDomain: "instagram-dev-58c0f.firebaseapp.com",
-//   projectId: "instagram-dev-58c0f",
-//   storageBucket: "instagram-dev-58c0f.appspot.com",
-//   messagingSenderId: "538988669227",
-//   appId: "1:538988669227:web:297c9cf74642a446db7d6e",
-//   measurementId: "G-2VMX4QGLD0",
-// };
-
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
